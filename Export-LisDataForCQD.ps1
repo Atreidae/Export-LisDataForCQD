4460b411434f91490c3bbb2470291e95ec95e1ed
James Arber <atreidae@chibiko.net>
2020-06-12 00:41:20 +1000

    Initial commit

---
+++ Export-LisDataForCQD.ps1
@@ -0,0 +1,365 @@
+﻿<#
+    .SYNOPSIS
+
+    This is a tool to export Lync / Skype for Business LIS data ready to import into the Office365 Call Quality Dashboard
+
+    .DESCRIPTION
+
+    Created by James Arber. www.UcMadScientist.com
+    
+    .NOTES
+
+    Version                : 1.0
+    Date                   : 11/06/2020 #todo
+    Lync Version           : Tested against Lync 2013, Skype4B 2015, Skype4b 2019
+    Author                 : James Arber
+    Header stolen from     : Greig Sheridan who stole it from Pat Richard's amazing "Get-CsConnections.ps1"
+
+
+    :v1.0: Initial Release
+
+    Disclaimer: Whilst I take considerable effort to ensure this script is error free and wont harm your enviroment.
+    I have no way to test every possible senario it may be used in. I provide these scripts free
+    to the Lync and Skype4B community AS IS without any warranty on it's appropriateness for use in
+    your environment. I disclaim all implied warranties including,
+    without limitation, any implied warranties of merchantability or of fitness for a particular
+    purpose. The entire risk arising out of the use or performance of the sample scripts and
+    documentation remains with you. In no event shall I be liable for any damages whatsoever
+    (including, without limitation, damages for loss of business profits, business interruption,
+    loss of business information, or other pecuniary loss) arising out of the use of or inability
+    to use the script or documentation.
+
+    Acknowledgements 	
+    : Testing and Advice
+    Greig Sheridan https://greiginsydney.com/about/ @greiginsydney
+
+    : Auto Update Code
+    Pat Richard https://ucunleashed.com @patrichard
+
+    : Proxy Detection
+    Michel de Rooij	http://eightwone.com
+
+    .LINK
+    https://www.UcMadScientist.com/preparing-for-teams-export-your-on-prem-lis-data-for-cqd/
+
+    .KNOWN ISSUES
+    Check https://github.com/Atreidae/Export-LisDataForCQD/issues/
+
+    .EXAMPLE
+    Exports current LIS data as a CQD compliant CSV in the current folder.
+    PS C:\> Export-LisDataForCQD.ps1
+
+#>
+
+[CmdletBinding(DefaultParametersetName = 'Common')]
+param
+(
+  [switch]$SkipUpdateCheck,
+  [String]$script:LogFileLocation = $null
+)
+
+If (!$script:LogFileLocation) 
+{
+  $script:LogFileLocation = $PSCommandPath -replace '.ps1', '.log'
+}
+
+#region config
+[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
+$StartTime                          = Get-Date
+$VerbosePreference                  = 'SilentlyContinue' #TODO
+[String]$ScriptVersion              = '0.1.0'
+[string]$GithubRepo                 = 'Export-LisDataForCQD'
+[string]$GithubBranch               = 'master' #todo
+[string]$BlogPost                   = 'https://www.UcMadScientist.com/preparing-for-teams-export-your-on-prem-lis-data-for-cqd/' 
+
+##Edit this stuff##
+[String]$Filename = "LIS-DataExport.csv"
+[String]$NetworkSize = 24 #24 bits by default, ie 255.255.255.0
+[String]$InsideCorp = "1" #True by default. Edit the CSV file if you have external networks in there.
+[String]$ExpressRoute = "0" #False by default. Set to true if all your networks use Express Route for some reason. (Use local breakout!)
+[String]$VPN = "0" #False by default, edit the CSV file if you have your VPN subnets defined for some reaons (VPN is bad!)
+
+##Finish editable section##
+
+Function Write-Log
+{
+  <#
+      .SYNOPSIS
+      Function to output messages to the console based on their severity and create log files
+
+      .DESCRIPTION
+      It's a logger.
+
+      .PARAMETER Message
+      The message to write
+
+      .PARAMETER Path
+      The location of the logfile.
+
+      .PARAMETER Severity
+      Sets the severity of the log message, Higher severities will call Write-Warning or Write-Error
+
+      .PARAMETER Component
+      Used to track the module or function that called "Write-Log" 
+
+      .PARAMETER LogOnly
+      Forces Write-Log to not display anything to the user
+
+      .EXAMPLE
+      Write-Log -Message 'This is a log message' -Severity 3 -component 'Example Component'
+      Writes a log file message and displays a warning to the user
+
+      .NOTES
+      N/A
+
+      .LINK
+      http://www.UcMadScientist.com
+
+      .INPUTS
+      This function does not accept pipelined input
+
+      .OUTPUTS
+      This function does not create pipelined output
+  #>
+  [CmdletBinding()]
+  PARAM
+  (
+    [String]$Message,
+    [String]$Path = $script:LogFileLocation,
+    [int]$Severity = 1,
+    [string]$Component = 'Default',
+    [switch]$LogOnly
+  )
+  $Date             = Get-Date -Format 'HH:mm:ss'
+  $Date2            = Get-Date -Format 'MM-dd-yyyy'
+  $MaxLogFileSizeMB = 10
+  
+  If(Test-Path -Path $Path)
+  {
+    if(((Get-ChildItem -Path $Path).length/1MB) -gt $MaxLogFileSizeMB) # Check the size of the log file and archive if over the limit.
+    {
+      $ArchLogfile = $Path.replace('.log', "_$(Get-Date -Format dd-MM-yyy_hh-mm-ss).lo_")
+      Rename-Item -Path ren -NewName $Path -Path $ArchLogfile
+    }
+  }
+         
+  "$env:ComputerName date=$([char]34)$Date2$([char]34) time=$([char]34)$Date$([char]34) component=$([char]34)$Component$([char]34) type=$([char]34)$Severity$([char]34) Message=$([char]34)$Message$([char]34)"| Out-File -FilePath $Path -Append -NoClobber -Encoding default
+  If (!$LogOnly) 
+  {
+    #If LogOnly is set, we dont want to write anything to the screen as we are capturing data that might look bad onscreen
+      
+      
+    #If the log entry is just Verbose (1), output it to verbose
+    if ($Severity -eq 1) 
+    {
+      "$Date $Message"| Write-Verbose
+    }
+      
+    #If the log entry is just informational (2), output it to write-host
+    if ($Severity -eq 2) 
+    {
+      "Info: $Date $Message"| Write-Host -ForegroundColor Green
+    }
+    #If the log entry has a severity of 3 assume it's a warning and write it to write-warning
+    if ($Severity -eq 3) 
+    {
+      "$Date $Message"| Write-Warning
+    }
+    #If the log entry has a severity of 4 or higher, assume it's an error and display an error message (Note, critical errors are caught by throw statements so may not appear here)
+    if ($Severity -ge 4) 
+    {
+      "$Date $Message"| Write-Error
+    }
+  }
+}
+
+Function Get-IEProxy
+{
+  $function = 'Get-IEProxy'
+  Write-Log -component $function -Message 'Checking for IE First Run' -severity 1
+  if ((Get-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').Property -NotContains 'ProxyEnable')
+  {
+    $null = New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -Name ProxyEnable -Value 0
+  }
+  
+
+  Write-Log -component $function -Message 'Checking for Proxy' -severity 1
+  If ( (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyEnable -ne 0)
+  {
+    $proxies = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').proxyServer
+    if ($proxies) 
+    {
+      if ($proxies -ilike '*=*')
+      {
+        return $proxies -replace '=', '://' -split (';') | Select-Object -First 1
+      }
+      
+      Else 
+      {
+        return ('http://{0}' -f $proxies)
+      }
+    }
+    
+    Else 
+    {
+      return $null
+    }
+  }
+  Else 
+  {
+    return $null
+  }
+}
+
+Function Get-ScriptUpdate 
+{
+  $function = 'Get-ScriptUpdate'
+  Write-Log -component $function -Message 'Checking for Script Update' -severity 1
+  Write-Log -component $function -Message 'Checking for Proxy' -severity 1
+  $ProxyURL = Get-IEProxy
+  
+  If ($ProxyURL)
+  
+  {
+    Write-Log -component $function -Message "Using proxy address $ProxyURL" -severity 1
+  }
+  
+  Else
+  {
+    Write-Log -component $function -Message 'No proxy setting detected, using direct connection' -severity 1
+  }
+
+  Write-Log -component $function -Message "Polling https://raw.githubusercontent.com/atreidae/$GithubRepo/$GithubBranch/version" -severity 1
+  $GitHubScriptVersion = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/atreidae/$GithubRepo/$GithubBranch/version" -TimeoutSec 10 -Proxy $ProxyURL -UseBasicParsing
+  
+  If ($GitHubScriptVersion.Content.length -eq 0) 
+  {
+    #Empty data, throw an error
+    Write-Log -component $function -Message 'Error checking for new version. You can check manually using the url below' -severity 3
+    Write-Log -component $function -Message $BlogPost -severity 3 
+    Write-Log -component $function -Message 'Pausing for 5 seconds' -severity 1
+    Start-Sleep -Seconds 5
+  }
+  else
+  {
+    #Process the returned data
+    #Symver support!
+    [string]$Symver = ($GitHubScriptVersion.Content)
+    $splitgitver = $Symver.split('.') 
+    $splitver = $ScriptVersion.split('.')
+    $needsupdate = $false
+    #Check for Major version
+
+    if ([single]$splitgitver[0] -gt [single]$splitver[0])
+    {
+      $Needupdate = $true
+      #New Major Build available, #Prompt user to download
+      Write-Log -component $function -Message 'New Major Version Available' -severity 3
+      $title = 'Update Available'
+      $Message = 'a major update to this script is available, did you want to download it?'
+    }
+
+    if (([single]$splitgitver[1] -gt [single]$splitver[1]) -and ([single]$splitgitver[0] -eq [single]$splitver[0]))
+    {
+      $Needupdate = $true
+      #New Major Build available, #Prompt user to download
+      Write-Log -component $function -Message 'New Minor Version Available' -severity 3
+      $title = 'Update Available'
+      $Message = 'a minor update to this script is available, did you want to download it?'
+    }
+
+    if (([single]$splitgitver[2] -gt [single]$splitver[2]) -and ([single]$splitgitver[1] -gt [single]$splitver[1]) -and ([single]$splitgitver[0] -eq [single]$splitver[0]))
+    {
+      $Needupdate = $true
+      #New Major Build available, #Prompt user to download
+      Write-Log -component $function -Message 'New Bugfix Available' -severity 3
+      $title = 'Update Available'
+      $Message = 'a bugfix update to this script is available, did you want to download it?'
+    }
+
+    If($Needupdate)
+    {
+      $yes = New-Object -TypeName System.Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes', `
+      'Update the installed PowerShell Module'
+
+      $no = New-Object -TypeName System.Management.Automation.Host.ChoiceDescription -ArgumentList '&No', `
+      'No thanks.'
+
+      $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+      $result = $host.ui.PromptForChoice($title, $Message, $options, 0) 
+
+      switch ($result)
+      {
+        0 
+        {
+          #User said yes
+          Write-Log -component $function -Message 'User opted to download update' -severity 1
+          #start $BlogPost
+          Repair-BsInstalledModules -ModuleName 'BounShell' -Operation 'Update'
+          Write-Log -component $function -Message 'Exiting Script' -severity 3
+          Pause
+          exit
+        }
+        #User said no
+        1 
+        {
+          Write-Log -component $function -Message 'User opted to skip update' -severity 1
+        }
+      }
+    }
+    
+    #We already have the lastest version
+    Else
+    {
+      Write-Log -component $function -Message 'Script is upto date' -severity 1
+    }
+  }
+}
+
+Write-Log -Message "Export-LisDataForCQD.ps1 Version $ScriptVersion" -severity 2
+
+
+#Get Proxy Details
+$ProxyURL = Get-IEProxy
+If ($ProxyURL) 
+{
+  Write-Log -Message "Using proxy address $ProxyURL" -severity 2
+}
+Else 
+{
+  Write-Log -Message 'No proxy setting detected, using direct connection' -severity 1
+}
+
+#Check for Script update
+if ($SkipUpdateCheck -eq $false) 
+{
+  Get-ScriptUpdate
+}
+
+
+#Get LIS data
+Write-Log -Message "Getting LIS data" -Severity 2
+$LISData = Get-CsLisSubnet
+
+#process that data
+ForEach ($LisSubnet in $LISData) 
+{
+  Write-Log -Message "Processing $($LisSubnet.subnet)" -Severity 2
+  $LisSubnet.Subnet + ","` + `       #NetworkIP
+  $LisSubnet.Description + "," + `   #NetworkName
+  $NetworkSize+ "," + `              #NetworkSize
+  $LisSubnet.Location + "," + `      #BuildingName
+  $LisSubnet.CompanyName + "," + `   #OwnershipType
+  $LisSubnet.Location + "," + `      #BuildingType
+  $LisSubnet.Location + "," + `      #BuildingOfficeType
+  $LisSubnet.City + "," + `          #City
+  $LisSubnet.PostalCode + "," + `    #ZipCode
+  $LisSubnet.Country + "," + `       #Country
+  $LisSubnet.State + "," + `         #State
+  $LisSubnet.State + "," + `         #Region
+  $InsideCorp + "," + `              #InsideCorp
+  $ExpressRoute + "," + `            #ExpressRoute
+  $VPN | Out-File -File $filename -Append -NoClobber -Encoding default
+}
+Write-Log -Message "Done" -Severity 2
---
+++ README.md
@@ -0,0 +1,15 @@
+# Export-LisDataForCQD
+
+Export-LisDataForCQD is a tool to allow you to export your existing Lync/Skype LIS data for import into the Office365 Call Quality Dashboard
+
+
+## Installation
+Download Export-LisDataForCQD.zip from the releases page and run on your Skype or Lync FrontEnd.
+More information is available over <a href="https://www.UcMadScientist.com/preparing-for-teams-export-your-on-prem-lis-data-for-cqd/">here</a> 
+
+## Known Issues
+The script currently doesnt check to see if the exported file exists before running, if it does it appennds to the existing file
+
+## Fork me!
+This script is free, open source and licensed under the MIT Licence. Feel free to view the source, fork it, raise issues and submit your improvements via pull requests.
+
